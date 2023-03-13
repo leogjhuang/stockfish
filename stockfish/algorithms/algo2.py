a8622f75a63acbcc591375d28b7bd0bc429e7c75
Jerry Li <jerry.limagic0302@gmail.com>
2023-03-12 21:00:45 -0400

    updated file structure

---
+++ stockfish/algorithms/algo2.py
@@ -0,0 +1,84 @@
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+
+"""
+Algo1 refactored
+"""
+class Algo2:
+    """
+    Contains the logic for the trading algorithm.
+    """
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+        # Initialize the method output dict as an empty dict
+        result = {}
+        # Iterate over all the keys (the available products) contained in the order depths
+        for product in state.order_depths.keys():
+
+            # Check if the current product is the 'PEARLS' product, only then run the order logic
+            if product == 'PEARLS':
+
+                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
+                order_depth: OrderDepth = state.order_depths[product]
+
+                # Initialize the list of Orders to be sent as an empty list
+                orders: list[Order] = []
+
+                self.buy_all(state, order_depth.sell_orders, product, orders)
+                self.sell_all(state, order_depth.buy_orders, product, orders)
+                # Add all the above orders to the result dict
+                result[product] = orders
+
+        return result
+
+    def buy_all(self, state: TradingState, orders: Dict[int, int], product: str, all_orders: list[Order]):
+        acceptable_price = 10000
+        # If statement checks if there are any SELL orders in the PEARLS market
+        if len(orders) > 0:
+            # Variable keeping track of how much room we have left to buy
+            # We can only buy up to 20 PEARLS at a time
+            # This variable is initialized to 20, and is decremented by the volume of each order
+            limit = 20 - state.position[product] if product in state.position else 20
+            # Go through every single sell order from lowest to acceptable_price - 1
+            for best_ask in range(min(orders.keys()), acceptable_price):
+                # Check if the current price has any orders
+                if best_ask in acceptable_price:
+                    # If there are orders, retrieve the volume
+                    best_ask_volume = min(-acceptable_price[best_ask], limit)
+                    # Send a BUY order at the price level of the ask, with the same quantity
+                    self.display_buy(best_ask_volume, best_ask)
+                    all_orders.append(Order(product, best_ask, best_ask_volume))
+                    limit -= best_ask_volume
+                    if limit <= 0:
+                        break
+
+    def sell_all(self, state: TradingState, orders: Dict[int, int], product: str, all_orders: list[Order]):
+        acceptable_price = 10000
+        if len(orders) != 0:
+            limit = state.position[product] + 20 if product in state.position else 20
+            # Go through every single buy order from highest to acceptable_price + 1
+            for best_bid in range(max(orders.keys()), acceptable_price, -1):
+                # Check if the current price has any orders
+                if best_bid in orders:
+                    # If there are orders, retrieve the volume
+                    best_bid_volume = min(orders[best_bid], limit)
+                    # Send a SELL order at the price level of the bid, with the same quantity
+                    self.display_sell(best_bid_volume, best_bid)
+                    all_orders.append(Order(product, best_bid, -best_bid_volume))
+                    limit -= best_bid_volume
+                    if limit <= 0:
+                        break
+    
+    def display_buy(self, best_volume: int, best_ask_or_bid: int):
+        self.display_buy_or_sell("BUY", best_volume, best_ask_or_bid)
+    
+    def display_sell(self, best_volume: int, best_ask_or_bid: int):
+        self.display_buy_or_sell("SELL", best_volume, best_ask_or_bid)
+
+    def display_buy_or_sell(self, key_string: str, best_volume: int, best_ask_or_bid: int):
+        print(key_string, str(best_volume) + "x", best_ask_or_bid)
--- stockfish/trader.py
+++ stockfish/trader.py
@@ -1,99 +1,10 @@
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
-
+from algorithms.algo1 import Algo1
 
 class Trader:
-    """
-    Contains the logic for the trading algorithm.
-    """
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        """
-        Only method required. It takes all buy and sell orders for all symbols as an input,
-        and outputs a list of orders to be sent
-        """
-        # Initialize the method output dict as an empty dict
-        result = {}
+        algo = Algo1()
+        return algo.run(state)
 
-        # Iterate over all the keys (the available products) contained in the order depths
-        for product in state.order_depths.keys():
-
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product == 'PEARLS':
-
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
-                order_depth: OrderDepth = state.order_depths[product]
-
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
-
-                # Define a fair value for the PEARLS.
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                acceptable_price = 10000
-
-                # If statement checks if there are any SELL orders in the PEARLS market
-                if len(order_depth.sell_orders) > 0:
-                    # Variable keeping track of how much room we have left to buy
-                    # We can only buy up to 20 PEARLS at a time
-                    # This variable is initialized to 20, and is decremented by the volume of each order
-                    limit = 20 - state.position[product] if product in state.position else 20
-                    # Go through every single sell order from lowest to acceptable_price - 1
-                    for best_ask in range(min(order_depth.sell_orders.keys()), acceptable_price):
-                        # Check if the current price has any orders
-                        if best_ask in order_depth.sell_orders:
-                            # If there are orders, retrieve the volume
-                            best_ask_volume = min(-order_depth.sell_orders[best_ask], limit)
-                            # Send a BUY order at the price level of the ask, with the same quantity
-                            print("BUY", str(best_ask_volume) + "x", best_ask)
-                            orders.append(Order(product, best_ask, best_ask_volume))
-                            limit -= best_ask_volume
-                            if limit <= 0:
-                                break
-
-                    # # Sort all the available sell orders by their price,
-                    # # and select only the sell order with the lowest price
-                    # best_ask = min(order_depth.sell_orders.keys())
-                    # best_ask_volume = order_depth.sell_orders[best_ask]
-
-                    # # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    # if best_ask < acceptable_price:
-
-                    #     # In case the lowest ask is lower than our fair value,
-                    #     # This presents an opportunity for us to buy cheaply
-                    #     # The code below therefore sends a BUY order at the price level of the ask,
-                    #     # with the same quantity
-                    #     # We expect this order to trade with the sell order
-                    #     print("BUY", str(-best_ask_volume) + "x", best_ask)
-                    #     orders.append(Order(product, best_ask, -best_ask_volume))
-
-                # The below code block is similar to the one above,
-                # the difference is that it finds the highest bid (buy order)
-                # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    limit = state.position[product] + 20 if product in state.position else 20
-                    # Go through every single buy order from highest to acceptable_price + 1
-                    for best_bid in range(max(order_depth.buy_orders.keys()), acceptable_price, -1):
-                        # Check if the current price has any orders
-                        if best_bid in order_depth.buy_orders:
-                            # If there are orders, retrieve the volume
-                            best_bid_volume = min(order_depth.buy_orders[best_bid], limit)
-                            # Send a SELL order at the price level of the bid, with the same quantity
-                            print("SELL", str(best_bid_volume) + "x", best_bid)
-                            orders.append(Order(product, best_bid, -best_bid_volume))
-                            limit -= best_bid_volume
-                            if limit <= 0:
-                                break
-                    # best_bid = max(order_depth.buy_orders.keys())
-                    # best_bid_volume = order_depth.buy_orders[best_bid]
-                    # if best_bid > acceptable_price:
-                    #     print("SELL", str(best_bid_volume) + "x", best_bid)
-                    #     orders.append(Order(product, best_bid, -best_bid_volume))
-
-                # Add all the above orders to the result dict
-                result[product] = orders
-
-                # Return the dict of orders
-                # These possibly contain buy or sell orders for PEARLS
-                # Depending on the logic above
-        return result
