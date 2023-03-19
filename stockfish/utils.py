a5e48acdc2badf194da76eaf2127e9ce1a3c7493
Leo Huang <leogjhuang@gmail.com>
2023-03-19 17:30:13 -0600

    Change util methods to static

--- stockfish/utils.py
+++ stockfish/utils.py
@@ -1,7 +1,10 @@
 import math
+from datamodel import Order
+
 
 class Util:
-    def get_best_bid(self, order_depth):
+    @staticmethod
+    def get_best_bid(order_depth):
         """
         Returns the best bid and its volume
         """
@@ -12,7 +15,8 @@ class Util:
         return best_bid, best_bid_volume
 
 
-    def get_best_ask(self, order_depth):
+    @staticmethod
+    def get_best_ask(order_depth):
         """
         Returns the best ask and its volume
         """
@@ -23,18 +27,20 @@ class Util:
         return best_ask, best_ask_volume
 
 
-    def get_mid_price(self, order_depth):
+    @staticmethod
+    def get_mid_price(order_depth):
         """
         Returns the mid price
         """
-        best_bid, _ = self.get_best_bid(order_depth)
-        best_ask, _ = self.get_best_ask(order_depth)
+        best_bid, _ = Util.get_best_bid(order_depth)
+        best_ask, _ = Util.get_best_ask(order_depth)
         if best_bid is None or best_ask is None:
             return None
         return (best_bid + best_ask) / 2
 
 
-    def get_moving_average(self, trades, window_size):
+    @staticmethod
+    def get_moving_average(trades, window_size):
         """
         Returns the moving average of the last window_size trades
         """
@@ -42,15 +48,18 @@ class Util:
         return sum(trade.price for trade in trades[-window_size:]) / window_size
 
 
-    def get_moving_std(self, trades, window_size):
+    @staticmethod
+    def get_moving_std(trades, window_size):
         """
         Returns the moving standard deviation of the last window_size trades
         """
         window_size = min(len(trades), window_size)
-        mean = self.get_moving_average(trades, window_size)
+        mean = Util.get_moving_average(trades, window_size)
         return math.sqrt(sum((trade.price - mean) ** 2 for trade in trades[-window_size:]) / window_size)
 
-    def get_vwap(self, orders):
+
+    @staticmethod
+    def get_vwap(orders):
         """
         orders = order_depth.buy_orders or order_depth.sell_orders
         """
@@ -61,3 +70,23 @@ class Util:
             weighted_sum += price * quantity
             quantity_sum += quantity
         return weighted_sum / quantity_sum if quantity_sum != 0 else 0
+
+
+    @staticmethod
+    def place_buy_order(product, orders, price, quantity):
+        """
+        Places a buy order
+        """
+        quantity = abs(quantity)
+        print("BUY", str(quantity) + "x", price)
+        orders.append(Order(product, price, quantity))
+
+
+    @staticmethod
+    def place_sell_order(product, orders, price, quantity):
+        """
+        Places a sell order
+        """
+        quantity = abs(quantity)
+        print("SELL", str(quantity) + "x", price)
+        orders.append(Order(product, price, -quantity))
