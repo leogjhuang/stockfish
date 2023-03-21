3eeab4207962d7a45602d5bf7f057d42c9efd2f2
Jerry Li <jerry.limagic0302@gmail.com>
2023-03-21 18:44:01 -0400

    prints products

--- stockfish/manual/round1.py
+++ stockfish/manual/round1.py
@@ -3,6 +3,12 @@ SNOWBALL = 1
 WASABI = 2
 PIZZA = 3
 
+dict = {
+    SHELLS : "SHELLS",
+    SNOWBALL : "SNOWBALL",
+    WASABI : "WASABI",
+    PIZZA : "PIZZA",
+}
 
 class Graph:
     def __init__(self, n):
@@ -34,16 +40,23 @@ class Graph:
         for v, w in self.edges[u]:
             pass
 
+def main():
+    graph = Graph(4)
+    graph.add_edge(SHELLS, SHELLS, 1.00, 1.00)
+    graph.add_edge(SNOWBALL, SNOWBALL, 1.00, 1.00)
+    graph.add_edge(WASABI, WASABI, 1.00, 1.00)
+    graph.add_edge(PIZZA, PIZZA, 1.00, 1.00)
+    graph.add_edge(SHELLS, SNOWBALL, 1.98, 0.48)
+    graph.add_edge(SHELLS, WASABI, 0.64, 1.49)
+    graph.add_edge(SHELLS, PIZZA, 1.34, 0.75)
+    graph.add_edge(SNOWBALL, WASABI, 0.31, 3.1)
+    graph.add_edge(SNOWBALL, PIZZA, 0.67, 1.45)
+    graph.add_edge(WASABI, PIZZA, 1.95, 0.5)
+    res = graph.dfs(SHELLS, 0)[1]
+    ls = []
+    for product in res:
+        ls.append(dict[product])
+    print(ls)
 
-graph = Graph(4)
-graph.add_edge(SHELLS, SHELLS, 1.00, 1.00)
-graph.add_edge(SNOWBALL, SNOWBALL, 1.00, 1.00)
-graph.add_edge(WASABI, WASABI, 1.00, 1.00)
-graph.add_edge(PIZZA, PIZZA, 1.00, 1.00)
-graph.add_edge(SHELLS, SNOWBALL, 1.98, 0.48)
-graph.add_edge(SHELLS, WASABI, 0.64, 1.49)
-graph.add_edge(SHELLS, PIZZA, 1.34, 0.75)
-graph.add_edge(SNOWBALL, WASABI, 0.31, 3.1)
-graph.add_edge(SNOWBALL, PIZZA, 0.67, 1.45)
-graph.add_edge(WASABI, PIZZA, 1.95, 0.5)
-print(graph.dfs(SHELLS, 0)[1])
+if __name__ == '__main__':
+    main()
